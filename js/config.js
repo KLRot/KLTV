cd8ab5b9ca6ff9e4f70b2f47f3388a229cc2552e
KLRot <caojinlong0@163.com>
2025-06-18 16:57:35 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -125,28 +125,28 @@ const API_SITES = {
     // 我们提供此警告，是希望您能认清这些内容的本质——它们是压迫和奴役的工具，而非娱乐。
     ckzy: {
         api: 'https://www.ckzy1.com',
-        name: 'CK资源',
-        adult: true
+        name: 'CK资源'
+        // adult: true
     },
     jkun: {
         api: 'https://jkunzyapi.com',
-        name: 'jkun资源',
-        adult: true
+        name: 'jkun资源'
+        // adult: true
     },
     bwzy: {
         api: 'https://api.bwzym3u8.com',
-        name: '百万资源',
-        adult: true
+        name: '百万资源'
+        // adult: true
     },
     souav: {
         api: 'https://api.souavzy.vip',
-        name: 'souav资源',
-        adult: true
+        name: 'souav资源'
+        // adult: true
     },
     r155: {
         api: 'https://155api.com',
-        name: '155资源',
-        adult: true
+        name: '155资源'
+        // adult: true
     },
     lsb: {
         api: 'https://apilsbzy1.com',
